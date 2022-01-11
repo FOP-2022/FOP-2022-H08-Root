8e372f1998770d6dbc07ae487406a842ee2a987a
Alexander Staeding <alexanderstaeding@gmail.com>
2022-01-11 20:36:37 +0100

    2 -> 4 spaces and add style plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,40 @@
 plugins {
-  java
+    java
+    id("org.sourcegrade.style") version "1.1.0"
 }
+
+tasks {
+    create<Jar>("graderJar") {
+        group = "build"
+        afterEvaluate {
+            archiveFileName.set("FOP-2022-H08-${project.version}.jar")
+            from(project(":grader").sourceSets.main.get().allSource)
+            from(project(":solution").sourceSets.main.get().allSource)
+        }
+    }
+}
+
 allprojects {
-  apply(plugin = "java")
-  repositories {
-    mavenCentral()
-  }
-  java {
-    withSourcesJar()
-  }
-  tasks {
-    jar {
-      archiveFileName.set("${rootProject.name}-${project.name}.jar")
+    apply(plugin = "java")
+    apply(plugin = "org.sourcegrade.style")
+    version = "1.0.0-SNAPSHOT"
+    repositories {
+        mavenCentral()
     }
-    named<Jar>("sourcesJar") {
-      archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
+    java {
+        withSourcesJar()
     }
-  }
+    tasks {
+        withType<JavaCompile> {
+            options.encoding = "UTF-8"
+            sourceCompatibility = "11"
+            targetCompatibility = "11"
+        }
+        jar {
+            archiveFileName.set("${rootProject.name}-${project.name}.jar")
+        }
+        named<Jar>("sourcesJar") {
+            archiveFileName.set("${rootProject.name}-${project.name}-sources.jar")
+        }
+    }
 }
